8598fefc537b8ca61c8fa386cc28db56db381de3
Dilan madhusanka <37908400+DilanMadhusanka@users.noreply.github.com>
2019-10-14 23:28:08 +0530

    create home page

---
+++ Beautiful-places-of-Rathnapura/src/main/java/com/places/details/Beautiful/places/of/Rathnapura/BeautifulPlacesOfRathnapuraApplication.java
@@ -0,0 +1,13 @@
+package com.places.details.Beautiful.places.of.Rathnapura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeautifulPlacesOfRathnapuraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeautifulPlacesOfRathnapuraApplication.class, args);
+	}
+
+}
---
+++ Beautiful-places-of-Rathnapura/src/test/java/com/places/details/Beautiful/places/of/Rathnapura/BeautifulPlacesOfRathnapuraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.places.details.Beautiful.places.of.Rathnapura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeautifulPlacesOfRathnapuraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
