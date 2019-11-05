77d0157b4485c4f984d09db237c8987e2fc6b565
Dilan madhusanka <37908400+DilanMadhusanka@users.noreply.github.com>
2019-11-05 16:46:17 +0530

    create RestPlacesService class
    For getting data from the database

---
+++ Beautiful-places-of-Rathnapura/src/main/java/com/places/details/Beautiful/places/of/Rathnapura/RestPlacesService.java
@@ -0,0 +1,24 @@
+package com.places.details.Beautiful.places.of.Rathnapura;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.places.details.Beautiful.places.of.Rathnapura.jpa.RestPlacesRepository;
+import com.places.details.Beautiful.places.of.Rathnapura.model.RestPlacesModel;
+
+public class RestPlacesService {
+	
+	@Autowired
+	RestPlacesRepository restPlacesRepository;
+
+	public List<RestPlacesModel> getRestPlacesDetails() {
+		List<RestPlacesModel> list = new ArrayList<>();
+		
+		for(RestPlacesModel restPlaceModel : restPlacesRepository.findAll()) {
+			list.add(restPlaceModel);
+		}
+		return list;
+	}
+}
